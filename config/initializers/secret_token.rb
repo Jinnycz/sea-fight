6ca5fc6f5ed72e790aad2e81ed684bd347dc2f3a
Phobos98 <phobos98@phobos98.net>
2013-03-20 10:50:35 +0200

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeaFight::Application.config.secret_token = '2de520c798ce28f44aa52108cbd1e03beed4b8f9fb2a892d94934503f4ce515e93270a2f11e6875b4e313e23664b0da687e42e15b5b1d0e185b31c1be4000cd3'
