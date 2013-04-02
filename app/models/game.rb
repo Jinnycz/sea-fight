0eaa1e7b91f6759343c2ee5395dbdd8b314e3fd8
Phobos98 <phobos98@phobos98.net>
2013-04-02 16:26:27 +0300

    Renamed game's user relation to player

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ActiveRecord::Base
   # attr_accessible :title, :body
 
-  has_and_belongs_to_many :users
+  has_and_belongs_to_many :players, class_name: "User"
   has_many :tables
 end
